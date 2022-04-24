96615800d15f194e81e806d5ccb8c376a4608fc5
danyaVKA <988maloivanov@internet.ru>
2022-04-24 22:50:48 +0300

    Initial commit

---
+++ src/main/java/ru/ivmiit/servlets/servlet_front.java
@@ -0,0 +1,8 @@
+package ru.ivmiit.servlets;
+import java.math.*;
+
+public class servlet_front {
+    public static void main (String[] args) {
+        System.out.println("tebe" + "pizda");
+    }
+}
